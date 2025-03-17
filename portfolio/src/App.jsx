3e88a744c5b23ff0cb2745b2d0e21a4d66e7a32e
snk9341 <ilanncappaert@gmail.com>
2025-03-18 00:20:06 +0100

    start project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './reset.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="background-menu">
+        <img src="https://images.steamusercontent.com/ugc/1667980476491829428/E7FB16BC376DAA7381FDC85AC32ACA6B13DA132E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true" className='image-background' alt="" />
+        <h1>mon portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

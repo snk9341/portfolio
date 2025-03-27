6d5c42216ba5afe73bf28e64334df0a86fa2c87b
snk9341 <ilanncappaert@gmail.com>
2025-03-27 20:22:20 +0100

    update portfolio

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -20,18 +20,6 @@ function SideMenu({}) {
             </a>
           </li>
           <li className="menuLI">
-            <a href="#mon-profil">
-              <div className="liSideBar">
-                <img
-                  className="liImg"
-                  src="https://img.icons8.com/office40/512/square.png  "
-                  alt=""
-                />
-                <h2 className="menuOption">Mon profil</h2>
-              </div>
-            </a>
-          </li>
-          <li className="menuLI">
             <a href="#experience-professionel">
               <div className="liSideBar">
                 <img
@@ -70,7 +58,11 @@ function SideMenu({}) {
           <li className="menuLI">
             <a href="#veille-technologique">
               <div className="liSideBar">
-                <img className="liImg" src="https://img.icons8.com/office40/512/square.png" alt=""/>
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png"
+                  alt=""
+                />
                 <h2 className="menuOption">Veille technologique</h2>
               </div>
             </a>
@@ -87,7 +79,7 @@ function Header({}) {
       <div className="myProfil">
         <img
           className="imgLogoSite"
-          src="https://img.icons8.com/office40/512/square.png"
+          src="https://cdn-icons-png.flaticon.com/512/4004/4004113.png"
           alt="logo application"
         />
         <h1 className="nameAppli">mon portefolio</h1>
@@ -96,22 +88,44 @@ function Header({}) {
   );
 }
 
+function Acceuil({}) {
+  return (
+    <>
+      <div id="acceuil">
+        <a href="#acceuil"><h2>Mon profil</h2></a>
+        <br />
+        <p className="pAcceuil">
+          Hello, moi c'est Ilann ! Je suis étudiant en BTS SIO option SLAM à
+          l'institut F2i de Vincennes, où je me spécialise dans le développement
+          d'applications. <br /> En parallèle, je travaille en alternance dans une
+          startup à Station F, un environnement dynamique qui me permet de
+          mettre en pratique mes compétences et d'en apprendre toujours plus. <br /><br />
+          Passionné par le code et les nouvelles technologies, j'aime créer des
+          applications fonctionnelles et bien pensées. <br /><br /> Que ce soit du
+          développement web, mobile ou logiciel, je suis toujours curieux de
+          découvrir de nouvelles choses et d'améliorer mes projets. <br /><br /> Ce portfolio
+          est là pour partager mon travail et montrer ce que je sais faire.
+          Bonne visite !
+        </p>
+      </div>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <>
-      <div className="website">
-        <div className="header">
-          <Header />
-        </div>
-        <div className="sidebar">
-          <div className="headerLandscape" onClick={() => test()}>
-            <Header />
+      <div className="landscape">
+        <div className="website">
+          <div className="sidebar">
+            <div className="headerLandscape" onClick={() => test()}>
+              <Header />
+            </div>
+            <SideMenu />
           </div>
-          <SideMenu />
-        </div>
-        <div className="portfolio">
-          <div id="acceuil"></div>
-          <div id="mon-profil"></div>
+          <div className="portfolio">
+            <Acceuil />
+          </div>
         </div>
       </div>
     </>

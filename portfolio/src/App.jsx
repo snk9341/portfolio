62ee9b3fa387a4a07735e4dfdc04d168911a2a97
snk9341 <ilanncappaert@gmail.com>
2025-03-27 18:20:45 +0100

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,19 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './reset.css'
-import './App.css'
+import { useState } from "react";
+import "./reset.css";
+import "./App.css";
 
-function App() {
-
+function SideMenu({}) {
   return (
     <>
-      <div className="background-menu">
-        <img src="https://images.steamusercontent.com/ugc/1667980476491829428/E7FB16BC376DAA7381FDC85AC32ACA6B13DA132E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true" className='image-background' alt="" />
-        <h1>mon portfolio</h1>
+      <div className="menu">
+        <ul className="menuUL">
+          <li className="menuLI">
+            <a href="#acceuil">
+              <div className="liSideBar">
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png"
+                  alt=""
+                />
+                <h2 className="menuOption">Acceuil</h2>
+              </div>
+            </a>
+          </li>
+          <li className="menuLI">
+            <a href="#mon-profil">
+              <div className="liSideBar">
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png  "
+                  alt=""
+                />
+                <h2 className="menuOption">Mon profil</h2>
+              </div>
+            </a>
+          </li>
+          <li className="menuLI">
+            <a href="#experience-professionel">
+              <div className="liSideBar">
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png  "
+                  alt=""
+                />
+                <h2 className="menuOption">Experience professionel</h2>
+              </div>
+            </a>
+          </li>
+          <li className="menuLI">
+            <a href="#compétences">
+              <div className="liSideBar">
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png  "
+                  alt=""
+                />
+                <h2 className="menuOption">Compétences</h2>
+              </div>
+            </a>
+          </li>
+          <li className="menuLI">
+            <a href="#mes-projets">
+              <div className="liSideBar">
+                <img
+                  className="liImg"
+                  src="https://img.icons8.com/office40/512/square.png  "
+                  alt=""
+                />
+                <h2 className="menuOption">Mes projets</h2>
+              </div>
+            </a>
+          </li>
+          <li className="menuLI">
+            <a href="#veille-technologique">
+              <div className="liSideBar">
+                <img className="liImg" src="https://img.icons8.com/office40/512/square.png" alt=""/>
+                <h2 className="menuOption">Veille technologique</h2>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+function Header({}) {
+  return (
+    <>
+      <div className="myProfil">
+        <img
+          className="imgLogoSite"
+          src="https://img.icons8.com/office40/512/square.png"
+          alt="logo application"
+        />
+        <h1 className="nameAppli">mon portefolio</h1>
+      </div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <div className="website">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="sidebar">
+          <div className="headerLandscape" onClick={() => test()}>
+            <Header />
+          </div>
+          <SideMenu />
+        </div>
+        <div className="portfolio">
+          <div id="acceuil"></div>
+          <div id="mon-profil"></div>
+        </div>
+      </div>
+    </>
+  );
+}
